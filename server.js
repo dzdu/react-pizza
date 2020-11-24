b65dbc98f56b97f9ed2c2576593fc33f4028089a
instdeadangel <513dragan@gmail.com>
2020-11-24 21:11:26 +0100

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-    static: './build',
+  static: './build',
 });
 
 const PORT = process.env.PORT || 3001;
@@ -11,5 +11,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT)
+  console.log('Server is running');
 });
